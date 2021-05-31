1ca7ac1704ac9016958d6152112edb16329a36f9
JawLikeCat <wang408090847@163.com>
2021-05-31 21:58:21 +0800

    add HomeWork04

--- BasicModule/src/com/Jaw/BasicCode/HomeWork04.java
+++ BasicModule/src/com/Jaw/BasicCode/HomeWork04.java
@@ -16,6 +16,7 @@ public class HomeWork04 {
             for (int i = 2; i < num ; i++) {
                 if (num % i == 0 ) {
                     flag = false;
+                    break;
                 }
             }
             if (flag) {
