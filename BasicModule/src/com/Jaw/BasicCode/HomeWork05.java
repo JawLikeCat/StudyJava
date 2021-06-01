f5c3e3bc506f062f9509979a215c1dae27e2d7ed
Jaw <wang408090847@163.com>
2021-06-01 17:57:30 +0800

    Commit

---
+++ BasicModule/src/com/Jaw/BasicCode/HomeWork05.java
@@ -0,0 +1,17 @@
+package com.Jaw.BasicCode;
+
+public class HomeWork05 {
+    /*
+        从键盘接收一个正整数，该正整数作为行数输出一下图形，eg:输入5
+                *
+               ***
+              *****
+             *******
+            *********
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}
