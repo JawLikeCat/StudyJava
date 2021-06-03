a63ed379e994d3d7bec883f70c0f10b17c0a5eb8
Jaw <wang408090847@163.com>
2021-06-03 17:45:25 +0800

    merge MethodHomeWork01

--- BasicModule/src/com/Jaw/MethodCode/MethodHomeWork01.java
+++ BasicModule/src/com/Jaw/MethodCode/MethodHomeWork01.java
@@ -5,24 +5,11 @@ public class MethodHomeWork01 {
         写一个方法，输出大于一个正整数n的最小质数
      */
     public static void main(String[] args) {
-        System.out.println(findPrimeNumber1(55));
+
         findPrimeNumber2(55);
-
+        findPrimeNumber3(55);
     }
 
-    public static int findPrimeNumber1(int n) {
-        int i = 0;
-        while (true) {
-            int j = 1;
-            i++;
-            do {
-                j++;
-            } while ((n + i) % j != 0);
-            if (j == (n+i)) {
-                return n + i;
-            }
-        }
-    }
 
     public static void findPrimeNumber2(int n) {
 
@@ -33,6 +20,13 @@ public class MethodHomeWork01 {
                 break;
             }
         }
+    }
+
+    public static void findPrimeNumber3(int n) {
+
+        while (!isPrimeNumber(++n)) {
+        }
+        System.out.println(n);
     }
 
     public static boolean isPrimeNumber(int num) {
